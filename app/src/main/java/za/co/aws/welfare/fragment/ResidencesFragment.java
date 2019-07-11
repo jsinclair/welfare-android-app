5f2d7c2802ee20408363d54b221729e6ab07f996
Zynique <zynique@overtake.co.za>
2019-07-11 20:02:36 +0200

    Started adding a search view.

--- app/src/main/java/za/co/aws/welfare/fragment/ResidencesFragment.java
+++ app/src/main/java/za/co/aws/welfare/fragment/ResidencesFragment.java
@@ -2,20 +2,46 @@ package za.co.aws.welfare.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import za.co.aws.welfare.R;
 
 public class ResidencesFragment extends Fragment {
 
+    private LinearLayout searchView;
+    private FloatingActionButton expandButton;
+    private Button searchButton;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.residences, container, false);
+        View v = inflater.inflate(R.layout.residences, container, false);
+        searchView = v.findViewById(R.id.search_menu);
+        searchButton = v.findViewById(R.id.search_button);
+        expandButton = v.findViewById(R.id.expand_button);
+        expandButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    searchView.setVisibility(View.VISIBLE);
+                    expandButton.setVisibility(View.GONE);
+            }
+        });
 
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: START SEARCH HERE> ONLY Hide search menu on successful search!!
+                searchView.setVisibility(View.GONE);
+                expandButton.setVisibility(View.VISIBLE);
+            }
+        });
+        return v;
     }
 }
