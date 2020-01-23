7dd4bf8c2e893eb016401bb850ca28fef9db4627
Zynique <zynique@overtake.co.za>
2020-01-23 16:15:18 +0200

    Added list.

--- app/src/main/java/za/co/aws/welfare/viewModel/RemindersViewModel.java
+++ app/src/main/java/za/co/aws/welfare/viewModel/RemindersViewModel.java
@@ -6,14 +6,21 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
 import java.util.Date;
+import java.util.List;
+
+import za.co.aws.welfare.dataObjects.ResidentAnimalDetail;
 
 public class RemindersViewModel extends AndroidViewModel {
 
     public MutableLiveData<Date> mDateSelected;
     public MutableLiveData<String> mNotes;
+    public MutableLiveData<List<ResidentAnimalDetail>> mAnimalList;
 
     public RemindersViewModel(Application application) {
         super(application);
         mNotes = new MutableLiveData<>();
+        mAnimalList = new MutableLiveData<>();
     }
+
+
 }
